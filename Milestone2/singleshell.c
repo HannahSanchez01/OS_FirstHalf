10049e5a7c7f99fd73a0804f97eb8f7286a5005f
Hannah Sanchez <hsanche5@student11.cse.nd.edu>
2023-02-09 12:33:26 -0500

    Milestone 2 Final push

--- Milestone2/singleshell.c
+++ Milestone2/singleshell.c
@@ -4,32 +4,48 @@
 #include <sys/wait.h>
 #include <signal.h>
 
+
 void SIGhandler(int sig)
 {
-    printf("Control-C was pressed. Exiting program.\n");
+	 /* Signal handling function */
+    printf("Control-C was pressed ... exiting\n");
     exit(0);
 }
+
 
 int main(void)
 {
     char command[64];
-    char c;
-    
+    int c, rc;
+   
+	 // Signaler
     signal(SIGINT, SIGhandler);
 
     printf("Execute? ");
     scanf("%64[^\n]", command); //get input; limit length to avoid overflow
-    while (((c=getchar()) != '\n') && (c != EOF)); //clear input buffer
 
-    if(fork()==0){ //child process
-        printf("Executing: %s\n", command);
-        system(command);
-        exit(0);
+	 // clear input buffer
+    while (((c=getchar()) != '\n') && (c != EOF)){}
+
+	 rc = fork();
+
+    if(rc==0){ // child process
+        	printf("Executing: %s\n", command);
+        	system(command);
+        	exit(0);
     }
-    else{
+
+	 else if(rc<0){ // fork failed; exit
+	 	fprintf(stderr, "fork failed\n");
+		exit(1);
+	 }
+
+    else{ // parent process
         wait(NULL);
         printf("Execution complete!\n");
     }
 
     return 0;
 }
+
+
