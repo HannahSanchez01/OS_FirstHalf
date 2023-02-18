905132e1f7f39b55ebc073d0fe9b35f44553ca75
Hannah Sanchez <hsanche5@student11.cse.nd.edu>
2023-02-17 19:31:37 -0500

    Level 1 finished

--- Project02/ndshell2.c
+++ Project02/ndshell2.c
@@ -5,9 +5,10 @@
 #include <sys/wait.h>
 #include <signal.h>
 
+int pid_list[10] = {0}; //array to store current processes
 int i = 0;
-void SIGhandler(int sig)
-{
+
+void SIGhandler(int sig){
 	 /* Signal handling function */
 	 if(sig == SIGINT){
    	 printf("Control-C was pressed ... exiting\n");
@@ -19,48 +20,67 @@ void SIGhandler(int sig)
 	}
 }
 
+void pid_add(int pid){
+	for(int j=0; j<10; j++){
+		if (pid_list[j] == 0){
+			pid_list[j] = pid;
+			break;
+		}
+	}
+}
+
+void pid_remove(int pid){
+	for(int j=0; j<10; j++){
+		if (pid_list[j] == pid){
+			pid_list[j] = 0;
+			break;
+		}
+	}
+}
+
 /* Start */
-void start(int* status_ptr)
-{
+int start(char **command){
 	int cpid;
 	int rc = fork();
 
-   if(rc==0){ // child process
+	if(rc==0){ // child process
 		cpid = getpid();
 		printf("ndshell: process %d started\n",cpid);
-      //printf("Executing: %s\n", command);
-      //system(command);
-      exit(0);
+      	//printf("Executing: %s\n", command);
+      	execvp(command[0], command);
+		printf("This shouldn't appear\n");
+		pid_remove(cpid);
+      	exit(0);
     }
 
-	 else if(rc<0){ // fork failed; exit
+	else if(rc<0){ // fork failed; exit
 	 	fprintf(stderr, "fork failed\n");
 		exit(1);
-	 }
+	}
 
     else{ // parent process
-		// check                     TODO
-       waitpid(-1, status_ptr, WNOHANG);   /// NONBLOCKING HERE???!!!!!
-        printf("Execution complete!\n");
+		pid_add(rc); // rc is child pid
     }
 
-	 return;
+	return rc; //return child pid so we know about it later
 }
 
 /* WAIT */
-void wait_func(int * status_ptr){
+void wait_func(int *status_ptr){
 	pid_t cpid = waitpid(-1, status_ptr, 0);
 		
 	if (cpid == -1){ // no children
 		printf("ndshell: No children.\n");
 	}
 
-	else if  (WIFEXITED(*status_ptr)){ // if true, normal
+	else if (WIFEXITED(*status_ptr)){ // if true, normal
 		printf("ndshell: process %d exited normally with status %d\n", cpid, WEXITSTATUS(*status_ptr) );
+		pid_remove(cpid);
 	}
 		
 	else if (WIFSIGNALED(*status_ptr)){// if true, abnormal
 		printf("ndshell: process %d exited abnormally with signal %d\n", cpid, WTERMSIG(*status_ptr));
+		pid_remove(cpid);
 	}
 
 	return;
@@ -77,76 +97,76 @@ void wait_for(int pid, int *status_ptr){
 
 	else if  (WIFEXITED(*status_ptr)){ // if true, normal
 		printf("ndshell: process %d was exited normally with status %d\n", cpid, WEXITSTATUS(*status_ptr));
+		pid_remove(cpid);
+	}
+
+	else if (WIFSIGNALED(*status_ptr)){// if true, abnormal
+		printf("ndshell: process %d exited abnormally with signal %d\n", cpid, WTERMSIG(*status_ptr));
+		pid_remove(cpid);
 	}
 
 	return;
 }
 
-/*
 // RUN //
-void run_func(int *status_ptr, int pid){
-	start(status_ptr);
-	wait_for(pid, status_ptr);
+void run_func(char **command, int *status_ptr){
+	int cpid = start(command);
+	wait_for(cpid, status_ptr);
 	
-
 	return;
 }
-*/
-
 
 int main(void)
 {
+	struct sigaction sigSetValue;
+	sigSetValue.sa_handler = SIGhandler;
 
-	 struct sigaction sigSetValue;
-	 sigSetValue.sa_handler = SIGhandler;
-
-	 int max = 64;
-	 char *token;
-	 char *words[100];
+	int max = 1000;
+	char *token;
+	char *words[100];
+	char *command[100];
     char buf[max];
-    int c, rc;
-	 int cpid, pid;
-	 int *status_ptr;
+	int pid;
+	int status;
    
-	 // Set up sigactions   ///control C and Kill.
-	 // Kill
-	 sigaction(SIGKILL, &sigSetValue, NULL);
+	// Set up sigactions   ///control C and Kill.
+	// Kill
+	sigaction(SIGKILL, &sigSetValue, NULL);
 
 	// does this need to be sigaction instead? TODO
-	 // Signaler C 
+	// Signaler C 
     //signal(SIGINT, SIGhandler);
-	 sigaction(SIGINT, &sigSetValue, NULL);
+	sigaction(SIGINT, &sigSetValue, NULL);
 
-	 int done = 0;
-	 while( !done) {
+	int done = 0;
+	while( !done) {
 
 
 	// TODO Trying to clear the buffer?
-	 if (buf){
-		printf("%s\n",buf);
-	 	memset(buf, '\0', max);
-	 }
+	if (buf[0]){
+		//printf("%s\n",buf);
+	 	memset(buf, '\0', (size_t) max);
+	} 
 
-	
-	 
-
-	 // Print a prompt to indicate input is ready to be accepted
+	// Print a prompt to indicate input is ready to be accepted
     printf("ndshell>  ");
     // Receive input
-	 if (fgets( buf, max, stdin) == NULL){
+	if (fgets( buf, max, stdin) == NULL){
 	 	printf("Did you get here?\n");
 		//Stop if fgets returns NULL
-	 }
-	 // Split the buf and receive the first as the command
-	 token = strtok(buf, " \t\n"); // split by spaces
-	 words[i] = malloc(30 * sizeof(char)); // allocate space for the command string
-	 strcpy(words[i], token); // first word is command
-	 // Keep splitting the buf
-	 while (token != NULL){
-		 i++;
+	}
+	// Split the buf and receive the first as the command
+	token = strtok(buf, " \t\n"); // split by spaces
+	i=0; //reset word counter
+	words[i] = malloc(30 * sizeof(char)); // allocate space for the command string
+	strcpy(words[i], token); // first word is command
+	// Keep splitting the buf
+	while ((token != NULL) && (i<=100)){
+		i++;
 	    words[i] = malloc(30 * sizeof(char)); // allocate space for string
-	    words[i] = strtok(NULL, " ");
-	 }
+	    token = strtok(NULL, " \t\n");
+		words[i] = token;
+	}
 	 
 	// TODO:
 	//  /* OPTION WORKING*/
@@ -162,42 +182,33 @@ int main(void)
 	//// DOESN'T LET THE SHELL KEEP RUNNING: make it nonblocking
 	//! START //!
 	else if (strncmp(words[0], "start", sizeof("start")) == 0){
-		start(status_ptr);
+		for (int j=1; j<i; j++){
+			command[j-1] = words[j];
+			command[j] = NULL;
+		}
+		start(command);
+		usleep(10000);
 	}
 	
 	// WAIT 
 	else if (strncmp(words[0], "wait", sizeof("wait")) == 0){
-		wait_func(status_ptr);
+		wait_func(&status);
 	}
 	
 	// WAITFOR //blocks, wait for specifc waitpid
 	else if (strncmp(words[0], "waitfor", sizeof("waitfor")) == 0){
 		pid = atoi(words[1]); // make the input an int from string
-		wait_for(pid, status_ptr);
+		wait_for(pid, &status);
 	}
-
-
 	
 	// RUN	
 	else if (strncmp(words[0], "run", sizeof("run")) == 0){
-		printf("%d\n",i);
-		//run_func(pid, status_ptr);
-		// make arg list
-/*
- 		
-		if (fork() == 0){ // child
-			int status_code = execvp(words[1], arg_list);
-			if (status_code == -1){
-				printf("Terminated incorrectly\n");
-				return -1;
-			}
+		for (int j=1; j<i; j++){
+			command[j-1] = words[j];
+			command[j] = NULL;
 		}
-		else{ // parent
-			
-		}
-*/
+		run_func(command, &status);
 	}
-
 
 	else if (strncmp(words[0], "logout", sizeof("logout")) == 0){
 		done = 1; 
@@ -223,9 +234,7 @@ int main(void)
 	// KILL
 	printf("ndshell: process %d started\n", pid);
 	printf("ndshell: process %d exited abnormally with signal %d: Killed.\n", pid, );
-
-
-*/
+	*/
 
 
 
